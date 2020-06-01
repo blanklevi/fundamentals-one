e9701e86b62fd2e21998b5fc823e49c9a2b3419e
LEVI ____ <blanklevi@gmail.com>
2020-06-01 14:46:45 -0700

    finished

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FundamentalsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num < 256)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+            int num1 = 1;
+            for (int i = num1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+            }
+        }
+    }
+}
